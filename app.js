c4b816af10737530d36ea8fa51cb1344e362cfee
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-11-27 23:48:35 +0300

    implemented proper error handling with the error handling middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const csrf = require('csurf');
 
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -20,6 +21,8 @@ app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
