7bcd3ad3be9c1f64b0324d4dce165507bf95d9ef
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-11-27 23:22:31 +0300

    added user signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
+const User = require('../models/user.model');
+
 function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
-function signup(req, res) {}
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup();
+
+  res.redirect('/login');
+}
 
 function getLogin(req, res) {
-  // ...
+  res.render('customer/auth/login');
 }
 
 module.exports = {
