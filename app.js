d9ebb1aa6205fe8abe23df89cbebf292bd4ab225
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-11-27 20:58:46 +0300

    added ejs & first views

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(authRoutes);
 
