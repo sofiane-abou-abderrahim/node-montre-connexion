338f3ce98808172bc8c50e4cf963c474cfdd4f55
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-11-28 17:04:16 +0300

    added the image upload functionality

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -6,7 +6,12 @@ function getNewProduct(req, res) {
   res.render('admin/products/new-product');
 }
 
-function createNewProduct() {}
+function createNewProduct(req, res) {
+  console.log(req.body);
+  console.log(req.file);
+
+  res.redirect('/admin/products');
+}
 
 module.exports = {
   getProducts: getProducts,
