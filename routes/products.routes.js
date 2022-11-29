04f6ae3c0e65b82b6afcb2e0d27d310a1ce8da67
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-11-29 14:51:31 +0300

    output products for customers

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const productsController = require('../controllers/products.controller');
+
 const router = express.Router();
 
-router.get('/products', function (req, res) {
-  res.render('customer/products/all-products');
-});
+router.get('/products', productsController.getAllProducts);
 
 module.exports = router;
